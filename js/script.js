4955dec64109b08c748dce2bbe731da907f9519d
Alexey <alexey.anasov@yandex.by>
2020-07-12 19:06:58 +0300

    верстка слайдера

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(function(){
+
+  $('.banner__section__slider').slick({
+    dots: true,
+    prevArrow: '<button class="banner__section__slider-btn banner__section__slider-btnprev"><img src="img/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button class="banner__section__slider-btn banner__section__slider-btnnext"><img src="img/arrow-right.svg" alt=""></button>'
+  });
+
+});
