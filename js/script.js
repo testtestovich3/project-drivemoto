63f33a6c743c02c7905683fd636418239ac64d3d
Alexey <alexey.anasov@yandex.by>
2020-10-08 16:22:31 +0300

    Адаптив блока Популярные товары

--- js/script.js
+++ js/script.js
@@ -37,11 +37,46 @@ $(function(){
   })
 
   // Слайдер популярных товаров
-  $('.product-slider').slick({
+  $(".product-slider").slick({
     slidesToShow: 4,
     slidesToScroll: 1,
-    prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="img/arrow-black-left.svg" alt=""></button>',
-    nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="img/arrow-black-right.svg" alt=""></button>'
+    prevArrow:
+      '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="img/arrow-black-left.svg" alt=""></button>',
+    nextArrow:
+      '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="img/arrow-black-right.svg" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 1301,
+        settings: {
+          arrows: false,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 1201,
+        settings: {
+          arrows: false,
+          dots: true,
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 870,
+        settings: {
+          arrows: false,
+          dots: true,
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 590,
+        settings: {
+          arrows: false,
+          dots: true,
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
 
   // Обращение к инпутам (стилизация фильтра)
